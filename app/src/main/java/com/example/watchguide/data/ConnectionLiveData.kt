05675d9cffa0959391e00d6f00b6d9ed70d2b63f
ADEV <adevhelps@gmail.com>
2024-03-01 18:00:15 +0100

    chore: migrate to Material 3

---
+++ app/src/main/java/com/example/watchguide/data/ConnectionLiveData.kt
@@ -0,0 +1,49 @@
+package com.example.watchguide.data
+
+import android.content.Context
+import android.content.Context.CONNECTIVITY_SERVICE
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET
+import android.net.NetworkRequest
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.example.watchguide.data.models.NetworkStates
+
+class ConnectionLiveData(context: Context) : MutableLiveData<String>() {
+
+    private lateinit var networkCallback: ConnectivityManager.NetworkCallback
+    private val connectivityManager =
+        context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+    private val validNetworks: MutableSet<Network> = HashSet()
+
+    override fun onActive() {
+        networkCallback = createNetworkCallback()
+        val networkRequest = NetworkRequest.Builder()
+            .addCapability(NET_CAPABILITY_INTERNET)
+            .build()
+        connectivityManager.registerNetworkCallback(networkRequest, networkCallback)
+    }
+
+    override fun onInactive() {
+        connectivityManager.unregisterNetworkCallback(networkCallback)
+    }
+
+    private fun createNetworkCallback() = object : ConnectivityManager.NetworkCallback() {
+
+        override fun onAvailable(network: Network) {
+            val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
+            val hasInternetCapability = networkCapabilities?.hasCapability(NET_CAPABILITY_INTERNET)
+            if (hasInternetCapability == true) {
+                validNetworks.add(network)
+            }
+            checkValidNetworks()
+        }
+    }
+
+    private fun checkValidNetworks() {
+        if (validNetworks.size == 1) {
+            postValue(NetworkStates.OnAvailable.name)
+        }
+    }
+}
